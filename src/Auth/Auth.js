c50da8f25ae114e6ca9977aa7ee8125c23af6008
sandeep chahal <sandeepchahal573@gmail.com>
2020-01-05 11:57:58 +0530

    added spinners for auth page

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -9,26 +9,33 @@ import auth from "../firebase.util";
 function Auth(props) {
   const [email, setEmail] = useState("ss@ss.com");
   const [pass, setPass] = useState("Sandeep12");
+  const [loading, setLoader] = useState(false);
 
   const handleLogin = () => {
+    setLoader(true);
     auth
       .signInWithEmailAndPassword(email, pass)
       .then(user => {
         props.login(user);
+        setLoader(false);
       })
       .catch(error => {
         alert(error.message);
+        setLoader(false);
       });
   };
   const handleSignup = () => {
+    setLoader(true);
     auth
       .createUserWithEmailAndPassword(email, pass)
       .then(user => {
         // localStorage.set("token", user.token);
         props.login(user);
+        setLoader(false);
       })
       .catch(error => {
         alert("nope");
+        setLoader(false);
       });
   };
 
@@ -38,26 +45,34 @@ function Auth(props) {
       <div className="auth-side"></div>
 
       <div className="auth">
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={pass}
-          onChange={e => setPass(e.target.value)}
-        />
-        <div className="btns">
-          <div className="signup" onClick={handleSignup}>
-            SignUp
+        {loading ? (
+          <div className="cssload-container">
+            <div className="cssload-whirlpool"></div>
           </div>
-          <div className="login" onClick={handleLogin}>
-            Login
+        ) : (
+          <div>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={pass}
+              onChange={e => setPass(e.target.value)}
+            />
+            <div className="btns">
+              <div className="signup" onClick={handleSignup}>
+                SignUp
+              </div>
+              <div className="login" onClick={handleLogin}>
+                Login
+              </div>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
