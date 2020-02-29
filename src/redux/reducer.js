b3f533449ec180c33d910db5f6a628c5efe6f8ca
sandeep-chahal <sandeepchahal573@gmail.com>
2020-02-29 16:51:02 +0530

    fixed bug when adding log

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,55 +1,58 @@
 const INITIAL_STATE = {
-  logged: false,
-  user: null,
-  income: 8000,
-  balance: 8000,
-  expense: 0,
-  logs: [],
-  loading: true
+	logged: false,
+	user: null,
+	income: 8000,
+	balance: 8000,
+	expense: 0,
+	logs: [],
+	loading: true
 };
 
 const Reducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case "setLoading": {
-      return {
-        ...state,
-        loading: action.payload
-      };
-    }
-    case "Login":
-      return {
-        ...state,
-        logged: true,
-        user: action.payload
-      };
-    case "addItem":
-      const logs = [...state.logs];
-      if (!logs[0]) logs[0] = { date: "Today", logs: [] };
-      logs[0].logs = [action.payload, ...logs[0].logs];
-      const balance = state.balance - action.payload.amount;
-      const expense = state.expense - action.payload.amount;
-      return {
-        ...state,
-        logs,
-        balance,
-        expense
-      };
-    case "addFetchedItems":
-      return {
-        ...state,
-        logs: action.payload.items,
-        expense: action.payload.expense,
-        balance: state.income - action.payload.expense
-      };
-    case "setIncome":
-      return {
-        ...state,
-        income: action.payload,
-        balance: action.payload - state.expense
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case "setLoading": {
+			return {
+				...state,
+				loading: action.payload
+			};
+		}
+		case "Login":
+			return {
+				...state,
+				logged: true,
+				user: action.payload
+			};
+		case "addItem":
+			const logs = [...state.logs];
+
+			if (!logs[0] || logs[0].date !== "Today") {
+				logs.unshift({ date: "Today", logs: [action.payload] });
+			} else logs[0].logs = [action.payload, ...logs[0].logs];
+
+			const balance = state.balance - action.payload.amount;
+			const expense = state.expense - action.payload.amount;
+			return {
+				...state,
+				logs,
+				balance,
+				expense
+			};
+		case "addFetchedItems":
+			return {
+				...state,
+				logs: action.payload.items,
+				expense: action.payload.expense,
+				balance: state.income - action.payload.expense
+			};
+		case "setIncome":
+			return {
+				...state,
+				income: action.payload,
+				balance: action.payload - state.expense
+			};
+		default:
+			return state;
+	}
 };
 
 export default Reducer;
