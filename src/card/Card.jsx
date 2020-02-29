9de539ea2cdb11ef26bb2c38999254cd323563af
sandeep-chahal <sandeepchahal573@gmail.com>
2020-02-29 16:52:25 +0530

    added - in balance if > 0

--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -6,47 +6,47 @@ import firebase from "../firebase.util";
 // import { setIncome } from "../redux/actions";
 
 const handleIncomeChange = (userUid, setIncome) => {
-  const res = prompt("enter income");
-  const value = Number(res);
-  if (!value) {
-    alert("Nope, Not Today!");
-    return;
-  }
-  console.log("object", userUid);
-  firebase
-    .database()
-    .ref(userUid)
-    .child("income")
-    .set(value)
-    .then(() => {
-      alert("done");
-    })
-    .catch(err => alert(err.message));
-  setIncome(value);
+	const res = prompt("enter income");
+	const value = Number(res);
+	if (!value) {
+		alert("Nope, Not Today!");
+		return;
+	}
+	console.log("object", userUid);
+	firebase
+		.database()
+		.ref(userUid)
+		.child("income")
+		.set(value)
+		.then(() => {
+			alert("done");
+		})
+		.catch(err => alert(err.message));
+	setIncome(value);
 };
 
 function Card(props) {
-  const expense = props.name === "expense";
-  const income = props.name === "income";
-  return (
-    <div className={props.name + " card"}>
-      <div className="details" style={income ? { marginBottom: "-20px" } : {}}>
-        <h3>{props.name}</h3>
-        <div className="amount" style={{ display: "inline-block" }}>
-          {expense ? "-" : ""}${Math.abs(props.amount)}
-        </div>
-        {income ? (
-          <img
-            src={icon}
-            alt="edit"
-            onClick={() => handleIncomeChange(props.userUid, props.setIncome)}
-            style={{ display: "inline-block", marginLeft: "10px" }}
-          ></img>
-        ) : null}
-      </div>
-      <div className={props.name + " icon"}></div>
-    </div>
-  );
+	const expense = props.name === "expense";
+	const income = props.name === "income";
+	return (
+		<div className={props.name + " card"}>
+			<div className="details" style={income ? { marginBottom: "-20px" } : {}}>
+				<h3>{props.name}</h3>
+				<div className="amount" style={{ display: "inline-block" }}>
+					{expense || props.amount < 0 ? "-" : ""}${Math.abs(props.amount)}
+				</div>
+				{income ? (
+					<img
+						src={icon}
+						alt="edit"
+						onClick={() => handleIncomeChange(props.userUid, props.setIncome)}
+						style={{ display: "inline-block", marginLeft: "10px" }}
+					></img>
+				) : null}
+			</div>
+			<div className={props.name + " icon"}></div>
+		</div>
+	);
 }
 
 // const
