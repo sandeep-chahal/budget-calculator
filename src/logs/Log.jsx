c82bd35ecedeafa0f1a0fc1c90da8aecbce0029d
sandeep-chahal <sandeepchahal573@gmail.com>
2020-03-02 20:00:41 +0530

    added remove btn

--- src/logs/Log.jsx
+++ src/logs/Log.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import "./log.styles.scss";
 
 function Log(props) {
-  return (
-    <div className="log">
-      <div className="left">
-        <div className={props.name}></div>
-        <div className="name">{props.name}</div>
-      </div>
-      <div className="right">-${props.amount}</div>
-    </div>
-  );
+	return (
+		<div className="log">
+			<div className="left">
+				<div className={props.name}></div>
+				<div className="name">{props.name}</div>
+			</div>
+			<div className="right">
+				<div className="amount">-${props.amount}</div>
+				<div className="close">
+					<span></span>
+					<span></span>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Log;
